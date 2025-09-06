c01e4723b59abceed30ba313abb00b7cb40ba0eb
leong8502 <126236078+leong8502@users.noreply.github.com>
2025-09-07 01:05:56 +0800

    minor changes

--- resources/views/TrainSelectionPage.blade.php
+++ resources/views/TrainSelectionPage.blade.php
@@ -202,6 +202,7 @@
 
 <script src="{{ asset('js/TrainSelect.js') }}" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
 flatpickr("#depart-date", {
     minDate: "today",
@@ -210,7 +211,7 @@ flatpickr("#depart-date", {
         if (dateStr) {
             document.getElementById('return-date').disabled = document.querySelector('input[name="booking_type"]').value === 'OneWay';
             flatpickr("#return-date", {
-                minDate: dateStr,
+                minDate: "today" || dateStr,
                 dateFormat: "M d, Y"
             });
         }
@@ -218,14 +219,10 @@ flatpickr("#depart-date", {
     disable: [
         function(date) {
             // Disable depart-date input for return journey selection
-            return {{ session('selected_journey') && request()->input('booking_type') == 'Return' ? 'true' : 'false' }};
+            return ;
         }
     ]
 });
-</script>
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
 document.addEventListener('DOMContentLoaded', () => {
     const successMessage = "{{ session('success') }}";
     const infoMessage = "{{ session('info') }}";
